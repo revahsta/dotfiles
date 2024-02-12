c976a57859a6ba6833ecab27e51119b7ec3ce4f0
Alexander Shaver <alex@shaver.io>
2024-02-12 18:39:51 -0500

    neovim: Install curl before vim-plug

--- install-nvim.sh
+++ install-nvim.sh
@@ -16,6 +16,13 @@ else
     sudo apt-get install neovim
 fi
 
+if command -v curl &> /dev/null
+then
+    echo "curl is already installed, skipping..."
+else
+    sudo apt-get install curl
+fi
+
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
